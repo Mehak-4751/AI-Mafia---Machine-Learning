49eb724f3b1485341d825cfd7a9fae3acdf8e624
Mehak-4751 <84508881+Mehak-4751@users.noreply.github.com>
2021-12-28 17:06:45 +0530

    Add files via upload

---
+++ Automate_tinder_or_Linkedin.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 16:58:50 2021
+
+@author: Manan garg
+"""
+
+#open anaconda prompt
+pip install pyautogui
+import pyautogui
+# Place your cursor on tinder like button
+pyautogui.position() #x=456 ,y=457
+
+#open git bash
+vi automate.py
+
+import pyautogui
+import time
+for i in range(21):
+    pyautogui.click(x=456,y=457)
+    time.sleep(0.4)
+~
+~
+~
+:wq #for save and quit
+:i #for editing
+
+python automate.py
